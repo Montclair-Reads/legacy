64d85e962a539203a4272a15fe548f9e2803a911
Montclair-Reads <123111761+Montclair-Reads@users.noreply.github.com>
2023-01-24 17:25:28 -0800

    test

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const issueInput = document.querySelector('input')
 issueForm.addEventListener("submit", e => {
     e.preventDefault()
     window.location.pathname = `/${issueInput.value}.pdf`
+    console.log(issueInput)
 })
