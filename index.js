419309c6220980324ab8461a0c636a4755b6400d
Montclair-Reads <123111761+Montclair-Reads@users.noreply.github.com>
2023-02-02 10:00:18 -0800

    replaced with html

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const issueInput = document.querySelector('input')
 issueForm.addEventListener("submit", e => {
     e.preventDefault()
     console.log(issueInput)
-    window.location.pathname = `/${issueInput.value}.pdf`
+    window.location.pathname = `/${issueInput.value}.html`
 })
